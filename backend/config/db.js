d781b1d9679144c862242d18e69929ba6c357a9a
Suraj Kumar332 <surajk71621@gmail.com>
2026-05-27 11:25:41 +0530

    test mongo connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,6 +4,7 @@ const  connectDB = async ()=> {
     try{
         await mongoose.connect(process.env.MONGO_URI);
         console.log("DB Connected");
+        console.log("MONGO URI:", process.env.MONGODB_URI);
     }catch(error){
         console.log("DB error", error);
     }
